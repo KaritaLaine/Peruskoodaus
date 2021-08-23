fd11f583dd1f2bbbf433ae891f3e01849e8af95c
Karita Laine <44746@edu.raseko.fi>
2021-08-23 14:05:06 +0300

    Käsitellään eri henkilötunnuksen osia

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ henkilotunnus = input('Anna henkilötunnus: ')
 # Muutetaan henkilötunnus isoihin kirjaimiin
 henkilotunnus = henkilotunnus.upper()
 
+# Sanakirja vuosisatakoodin selvittämiseen
+vuosisata = {'+': 1800, '-': 1900, 'A': 2000}
+
 # Sanakirja tarkisteiden hakemiseen
 tarkisteet = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
               5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A',
@@ -14,6 +17,19 @@ tarkisteet = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
               23: 'R', 24: 'S', 25: 'T', 26: 'U', 27: 'V', 28: 'W',
               29: 'X', 30: 'Y'}
 
+# Erotetaan henkilötunnuksen osat omiin muuttujiin (merkkijonoja)
+paivatString = henkilotunnus[0] + henkilotunnus[1]
+kuukaudetString = henkilotunnus[2:4]
+vuodetString = henkilotunnus[4:6]
+
+# Muutetaan merkkijonot numeroiksi
+paivat = int(paivatString)
+kuukaudet = int(kuukaudetString)
+vuodet = int(vuodetString)
+
+# Tulostetaan eri henkilötunnuksen osat (päivä, kuukausi, vuosi)
+print('Päivät:', paivatString, 'Kuukaudet:',
+      kuukaudetString, 'Vuosi:', vuodetString)
 
 # Tulostetaan syötetty henkilötunnus koneen ruudulle
-print('Antamasi henkilötunnus oli', henkilotunnus)
+# print('Antamasi henkilötunnus oli', henkilotunnus)
