0c2442947e1f72a625cd256289624944e24ba255
Karita Laine <44746@edu.raseko.fi>
2021-08-23 11:42:29 +0300

    Muutettiin henkilötunnus isoihin kirjaimiin

---
+++ main.py
@@ -0,0 +1,10 @@
+# HENKILÖTUNNUKSEN TARKISTUSSOVELLUS
+
+# Kysytään käyttäjältä henkilötunnus - merkkijono (string)
+henkilotunnus = input("Anna henkilötunnus: ")
+
+# Muutetaan henkilötunnus isoihin kirjaimiin
+henkilotunnus = henkilotunnus.upper()
+
+# Tulostetaan syötetty henkilötunnus koneen ruudulle
+print("Antamasi henkilötunnus oli", henkilotunnus)
