aaa58e8c3afbc670d3b4206be22dc39b8ea42f20
Karita Laine <44746@edu.raseko.fi>
2021-08-25 14:43:39 +0300

    Modulien ja kirjastojen latausta

--- hetutarkistus.py
+++ hetutarkistus.py
@@ -10,7 +10,7 @@ def tarkista_hetu(hetu):
     Returns:
         boolean: True, jos oikein, False, jos väärin
     """
-    return oikein
+
 
     # Muutetaan henkilötunnus isoihin kirjaimiin
     hetu = hetu.upper()
@@ -20,11 +20,7 @@ def tarkista_hetu(hetu):
 
     # Sanakirja tarkisteiden hakemiseen
     tarkisteet = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
-                  5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A',
-                  11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'H',
-                  17: 'J', 18: 'K', 19: 'L', 20: 'M', 21: 'N', 22: 'P',
-                  23: 'R', 24: 'S', 25: 'T', 26: 'U', 27: 'V', 28: 'W',
-                  29: 'X', 30: 'Y'}
+                  5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'H', 17: 'J', 18: 'K', 19: 'L', 20: 'M', 21: 'N', 22: 'P', 23: 'R', 24: 'S', 25: 'T', 26: 'U', 27: 'V', 28: 'W', 29: 'X', 30: 'Y'}
 
     # Erotetaan henkilötunnuksen osat omiin muuttujiin (merkkijonoja)
     paivatString = hetu[0] + hetu[1]
@@ -35,8 +31,7 @@ def tarkista_hetu(hetu):
     tarkisteString = hetu[10]
 
     # Yhdistetään merkkijonot luvuksi tarkisteen laskentaa varten 130 728 478
-    luvutString = paivatString + kuukaudetString + \
-        vuodetString + jarjestysnumeroString
+    luvutString = paivatString + kuukaudetString + vuodetString + jarjestysnumeroString
 
     # Muutetaan se numeroksi (int)
     luvut = int(luvutString)
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # PÄÄOHJELMA
 
 # Modulien ja kirjastojen lataukset
+import hetutarkistus as ht 
+
+# Kysytään käyttäjältä henkilötunnus
+kysytty_hetu = input('Anna henkilötunnus: ')
+
+# Tarkistetaan onko hetu oikein
+oli_oikein = ht.tarkista_hetu(kysytty_hetu)
+
+# Ilmoitetaan käyttäjälle onko hetu oikein
+if oli_oikein == True:
+    print('Henkilötunnus OK.')
+else:
+    print('Henkilötunnus väärin, tarkista!')
