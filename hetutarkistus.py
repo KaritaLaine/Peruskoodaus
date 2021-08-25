49cb0a3459aeba035272ab9494784e1f1a36a4a2
Karita Laine <44746@edu.raseko.fi>
2021-08-25 14:26:00 +0300

    Talteen tehty koodi

---
+++ hetutarkistus.py
@@ -0,0 +1,56 @@
+# HENKILÖTUNNUKSEN TARKISTUSRUTIINI
+
+def tarkista_hetu(hetu):
+    """Tarkistaa, että henkilötunnus on oikein muodostettu
+    käyttäen modulo 31 tarkistetta
+
+    Args:
+        hetu (string): Suomalainen henkilötunnus merkkijonona
+
+    Returns:
+        boolean: True, jos oikein, False, jos väärin
+    """
+    return oikein
+
+    # Muutetaan henkilötunnus isoihin kirjaimiin
+    hetu = hetu.upper()
+
+    # Sanakirja vuosisatakoodin selvittämiseen
+    vuosisadat = {'+': 1800, '-': 1900, 'A': 2000}
+
+    # Sanakirja tarkisteiden hakemiseen
+    tarkisteet = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
+                  5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A',
+                  11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'H',
+                  17: 'J', 18: 'K', 19: 'L', 20: 'M', 21: 'N', 22: 'P',
+                  23: 'R', 24: 'S', 25: 'T', 26: 'U', 27: 'V', 28: 'W',
+                  29: 'X', 30: 'Y'}
+
+    # Erotetaan henkilötunnuksen osat omiin muuttujiin (merkkijonoja)
+    paivatString = hetu[0] + hetu[1]
+    kuukaudetString = hetu[2:4]
+    vuodetString = hetu[4:6]
+    vuosisatakoodiString = hetu[6]
+    jarjestysnumeroString = hetu[7:10]
+    tarkisteString = hetu[10]
+
+    # Yhdistetään merkkijonot luvuksi tarkisteen laskentaa varten 130 728 478
+    luvutString = paivatString + kuukaudetString + \
+        vuodetString + jarjestysnumeroString
+
+    # Muutetaan se numeroksi (int)
+    luvut = int(luvutString)
+
+    # Lasketaan jakojäännös 31:llä jaettuna (130829478 % 31) eli modulo 31
+    jakojaannos = luvut % 31
+
+    # Haetaan jakojäännöstä vastaava kirjain tarkisteet-sanakirjasta
+    uusiLaskettuTarkiste = tarkisteet[jakojaannos]
+
+    # Tarkistetaan onko laskettu tarkiste sama kuin syötetty tarkiste
+    if uusiLaskettuTarkiste == tarkisteString:
+        oikein = True
+    else:
+        oikein = False
+
+    return oikein
