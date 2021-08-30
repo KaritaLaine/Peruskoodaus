ea821fd196ee182515b5a644fbe9492107223d28
Karita Laine <44746@edu.raseko.fi>
2021-08-30 19:21:57 +0300

    Uusi versio hetutarkistuksesta
    korjattu versio :)

---
+++ v2 main.py
@@ -0,0 +1,17 @@
+import stdnum.fi.hetu
+
+import v2
+ 
+def main():
+     while True:
+         hetu = input('Anna HETU: ')
+         if stdnum.fi.hetu.is_valid(hetu):
+            print('Oikeanlainen HETU')
+            print(v2.sukupuoli(hetu))
+            print('Syntymäpäivä:', v2.syntymapaiva(hetu))
+            break
+         else:
+            print('Vääränlainen HETU')
+
+if __name__== '__main__':
+    main()
